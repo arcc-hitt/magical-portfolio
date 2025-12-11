d3efc913fa9f05cc4c8f052ffb712247644f2245
Archit Mahule <archit.mahule@mathongo.com>
2025-12-11 23:49:37 +0530

    feat: update background gradient and shadow effects for Stage component

--- src/components/figma/Stage.tsx
+++ src/components/figma/Stage.tsx
@@ -7,7 +7,7 @@ interface CurtainRectangleRedProps {
 const Stage: React.FC<CurtainRectangleRedProps> = ({ className = '' }) => {
   return (
     <div
-      className={`w-full h-4/7 bg-bg-stage absolute bottom-0 blur-md shadow-none ${className}`}
+      className={`w-full h-4/7 bg-bg-stage absolute bottom-0 blur-md shadow-[0px_4px_4px_10px_rgba(42,13,83,0.8)] backdrop-blur-sm ${className}`}
     />
   );
 };
